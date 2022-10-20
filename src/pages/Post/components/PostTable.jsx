be8bb21173c2fe364436d8d212f8c473231104d0
Roman Shiryaev <roman.shiryaev.ru@gmail.com>
2022-10-20 09:58:36 +0300

    Develop commit

--- src/pages/Post/components/PostTable.jsx
+++ src/pages/Post/components/PostTable.jsx
@@ -10,44 +10,54 @@ const PostTable = (props) => {
     <>
       <Table>
         <div className="table-item">
+          <div className="table-title__name">Метки</div>
           <div className="table-title__name">АПК</div>
           <div className="table-title__name">Дата</div>
-          <div className="table-title__name">Метки</div>
           <div className="table-title__name">Длительность</div>
           <div className="table-title__name">Система связи</div>
-          <div className="table-title__name">Пост регистрации</div>
+          <div className="table-title__name">Формат данных</div>
+          <div className="table-title__name">Трафик</div>
+          <div className="table-title__name">Коммент</div>
+
           <div className="table-title__name">IMEI</div>
           <div className="table-title__name">IMSI</div>
+          <div className="table-title__name">TMSI</div>
           <div className="table-title__name">ТФОП аб.</div>
-          <div className="table-title__name">ТФОП кор.</div>
-          <div className="table-title__name">Уст-во аб.</div>
-          <div className="table-title__name">Уст-во кор.</div>
           <div className="table-title__name">Направление</div>
-          <div className="table-title__name">Тип</div>
-          <div className="table-title__name">Point1</div>
-          <div className="table-title__name">Point2</div>
+          <div className="table-title__name">ТФОП коор</div>
+
+          {/* <div className="table-title__name">Уст-во аб.</div> */}
+          {/* <div className="table-title__name">Уст-во кор.</div> */}
+          {/* <div className="table-title__name">Тип</div> */}
+          <div className="table-title__name">Координаты</div>
+          {/* <div className="table-title__name">Point2</div> */}
+          <div className="table-title__name">Пост регистрации</div>
         </div>
-
-
         
         {props.postList.length !== 0 ? props.postList.map((item) => (
           <div className="table-item" key={item.id}>
+            <div className="table-info__name">{item.labels.map((item, index) => (<div key={index}>{item.name}</div>))}</div>
             <div className="table-info__name">{item.origin}</div>
             <div className="table-info__name">{item.time}</div>
-            <div className="table-info__name">{item.labels.map((item, index) => (<div key={index}>{item.name}</div>))}</div>
             <div className="table-info__name">{item.duration}</div>
             <div className="table-info__name">{item.sys}</div>
-            <div className="table-info__name">{item.regStation}</div>
+            <div className="table-info__name">{item.type}</div>
+            <div className="table-info__name">{item.traffic !== 0 && '+'}</div>
+            <div className="table-info__name">{item.comment}</div>
+
             <div className="table-info__name">{item.cc3}</div>
             <div className="table-info__name">{item.cc4}</div>
+            <div className="table-info__name">{item.cc5}</div>
             <div className="table-info__name">{item.cx1}</div>
-            <div className="table-info__name">{item.cx1b}</div>
-            <div className="table-info__name">{item.cx2}</div>
-            <div className="table-info__name">{item.cx2b}</div>
             <div className="table-info__name">{item.direction}</div>
-            <div className="table-info__name">{item.ot}</div>
+            <div className="table-info__name">{item.cx1b}</div>
+
+            {/* <div className="table-info__name">{item.cx2}</div> */}
+            {/* <div className="table-info__name">{item.cx2b}</div> */}
+            {/* <div className="table-info__name">{item.ot}</div> */}
             <div className="table-info__name">{item.coordinates[0].point1}</div>
-            <div className="table-info__name">{item.coordinates[0].point2}</div>
+            {/* <div className="table-info__name">{item.coordinates[0].point2}</div> */}
+            <div className="table-info__name">{item.regStation}</div>
           </div>
         )) : <Loader/>}
 
